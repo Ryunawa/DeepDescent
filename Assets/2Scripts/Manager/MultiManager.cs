ca537b81a24a3c3d09f1d7a5a45727ad8857fe86
MrNyvle <elvynwakeford@gmail.com>
2024-05-27 19:18:36 +0200

    Revert "removed rpc calls in MultiManager.cs, needs to inherit NetworkBehaviour to work"
    This reverts commit 41384fc0a9db55d54c955e32c6d8d4dd0a9f201c.

--- Assets/2Scripts/Manager/MultiManager.cs
+++ Assets/2Scripts/Manager/MultiManager.cs
@@ -544,4 +544,21 @@ public class MultiManager : Singleton<MultiManager>
             			position: position, rotation: rotation);
 		}
 	}
+	
+	[Rpc(SendTo.Server, RequireOwnership = false)]
+	public void SpawnNetworkObjectRpc(NetworkObject objectToSpawn, Vector3 position, Quaternion rotation)
+	{
+		objectToSpawn.InstantiateAndSpawn(NetworkManager.Singleton, NetworkManager.Singleton.LocalClientId,
+			position: position, rotation: rotation);
+	}
+
+	[Rpc(SendTo.Server, RequireOwnership = false)]
+	public void DespawnNetworkObjectRpc(NetworkObject objectToDespawn)
+	{
+		objectToDespawn.Despawn();
+		if (objectToDespawn.gameObject)
+		{
+			Destroy(objectToDespawn.gameObject);
+		}
+	}
 }
