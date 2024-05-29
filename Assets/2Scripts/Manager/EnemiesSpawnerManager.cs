2a01e4116659943c2642222d41fd1bfa111032b4
Robin DOUET <robin.douet@gmail.com>
2024-05-29 15:40:22 +0200

    Rework enemy spawn, added spawn points in each room

--- Assets/2Scripts/Manager/EnemiesSpawnerManager.cs
+++ Assets/2Scripts/Manager/EnemiesSpawnerManager.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 using Unity.Mathematics;
 using Unity.Netcode;
 using UnityEngine.AI;
+using UnityEngine.Serialization;
 using static _2Scripts.Helpers.StructureAccessMethods;
 using Random = UnityEngine.Random;
 
@@ -30,8 +31,9 @@ namespace _2Scripts.Manager
         
         [SerializeField] private List<LevelData> spawnableEnemiesPrefabsByLevel;
         [SerializeField] private int maxEnemiesPerLevel = 5;
+        [SerializeField] private int maxEnemiesPerRoom = 3;
         [Range(0.1f, 10)]
-        [SerializeField] private float spawnIntervalInSecond = 2f;
+        [SerializeField] private float spawnIntervalInSecond;
 
         [SerializeField] private GameObject spawnParticle;
 
@@ -40,7 +42,13 @@ namespace _2Scripts.Manager
         private int _currLevel = 1; // Depend on the game manager
         
         #endregion
-        
+
+        protected override void Awake()
+        {
+            base.Awake();
+            LevelGenerator.instance.dungeonGeneratedEvent.AddListener(DEBUG_StartSpawn);
+        }
+
         private void Start()
         {
             _enemiesList = DifficultyManager.instance.GetEnemiesStatsToUse();
@@ -75,15 +83,18 @@ namespace _2Scripts.Manager
         /// Return the position of a random room to use to spawn the enemy
         /// </summary>
         /// <returns></returns>
-        private int GetRandomRoomToSpawnIn()
+        private (Room,List<GameObject>) GetRandomRoomToSpawnIn()
         {
-            int randomInt;
+            (Room,List<GameObject>) randomRoom;
+            List<(Room, List<GameObject>)> roomsTuple = LevelGenerator.instance.GetEnemySpawnPoints();
+            int roomIndex;
             do
             {
-                randomInt = Random.Range(0, LevelGenerator.instance.dungeon.Length);
-            } while (LevelGenerator.instance.IsRoomEmpty(randomInt) && LevelGenerator.instance.dungeon[randomInt].enemiesCount <= 3);
-
-            return randomInt;
+                int randomInt = Random.Range(0, roomsTuple.Count);
+                randomRoom = roomsTuple[randomInt];
+                roomIndex = LevelGenerator.instance.GetIndexOfRoom(randomRoom.Item1);
+            } while (LevelGenerator.instance.IsRoomEmpty(roomIndex) || randomRoom.Item1.enemiesCount >= maxEnemiesPerRoom);
+            return randomRoom;
         }
 
         /// <summary>
@@ -99,10 +110,11 @@ namespace _2Scripts.Manager
                 if (_currentEnemiesCount < maxEnemiesPerLevel)
                 {
                     EnemyStats objectToSpawn = ChooseEnemyToSpawn();
-                    int roomToSpawnInIndex = GetRandomRoomToSpawnIn();
-                    Vector3 spawningPosition = LevelGenerator.instance.GetPosition(roomToSpawnInIndex);
-                    Room roomToSpawnInObject = LevelGenerator.instance.dungeon[roomToSpawnInIndex];
+                    (Room,List<GameObject>) roomToSpawnIn = GetRandomRoomToSpawnIn();
+                    Vector3 spawningPosition = roomToSpawnIn.Item2[Random.Range(0, roomToSpawnIn.Item2.Count)].transform.position;
 
+                    OnEnemiesSpawnedOrKilledEventHandler?.Invoke(roomToSpawnIn.Item1, 1);
+                    
                     if (bSpawnInMultiplayer)
                     {
                          MultiManager.instance.SpawnNetworkObject(objectToSpawn.enemyPrefab.GetComponent<NetworkObject>(),
@@ -112,31 +124,34 @@ namespace _2Scripts.Manager
                     else
                     {
                         //DEBUG ONLY
-                        var newEnemy = Instantiate(objectToSpawn.enemyPrefab, new Vector3(spawningPosition.x, -1, spawningPosition.z),
+                        GameObject newEnemy = Instantiate(objectToSpawn.enemyPrefab, new Vector3(spawningPosition.x + 5, -1, spawningPosition.z),
                             quaternion.identity);
                         
                         EnemyData newEnemyData = newEnemy.GetComponent<EnemyData>();
                         newEnemyData.enemyStats = objectToSpawn;
-                        newEnemyData.roomSpawnedInID = roomToSpawnInObject.ID;
+                        newEnemyData.roomSpawnedInID = roomToSpawnIn.Item1.ID;
                         
                         newEnemy.GetComponent<AIController>().enabled = false;
-                        
                         Vector3 newEnemyPosition = newEnemy.transform.position;
-
-                        yield return new WaitForSeconds(0.2f);
-                        GameObject newParticle = Instantiate(spawnParticle, new Vector3(newEnemyPosition.x, 0, newEnemyPosition.z - 0.5f),
-                            quaternion.identity);
-                        
-                        newParticle.transform.localScale *= 2.5f;
-                        
-                        yield return new WaitForSeconds(10f);
-                        Destroy(newParticle);
-                        newEnemy.GetComponent<AIController>().enabled = true;
+                        StartCoroutine(StartSpawnAnim(newEnemyPosition, newEnemy));
                     }
                     _currentEnemiesCount++;
-                    OnEnemiesSpawnedOrKilledEventHandler?.Invoke(roomToSpawnInObject, 1);
                 }
             }
+        }
+
+        IEnumerator StartSpawnAnim(Vector3 pEnemyPosition, GameObject pEnemyGameObject)
+        {
+            yield return new WaitForSeconds(0.2f);
+            GameObject newParticle = Instantiate(spawnParticle, new Vector3(pEnemyPosition.x, 0.2f, pEnemyPosition.z - 0.5f),
+                quaternion.identity);
+                        
+            newParticle.transform.localScale *= 2.5f;
+                        
+            yield return new WaitForSeconds(pEnemyGameObject.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + 0.25f);
+                        
+            Destroy(newParticle);
+            pEnemyGameObject.GetComponent<AIController>().enabled = true;
         }
         
         /// <summary>
