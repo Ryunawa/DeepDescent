21e84f5e6da1d5ea5df9799a42d147099843d2c5
MrNyvle <elvynwakeford@gmail.com>
2024-06-10 16:02:04 +0200

    added character selection to join

--- Assets/2Scripts/Manager/MultiManager.cs
+++ Assets/2Scripts/Manager/MultiManager.cs
@@ -37,7 +37,8 @@ public class MultiManager : Singleton<MultiManager>
 	public UnityEvent lobbyCreated;
 	public UnityEvent lobbyJoined;
 	public UnityEvent kickedEvent;
-	public UnityEvent refreshUI;
+	public UnityEvent<bool> refreshUI;
+	public UnityEvent CharacterChosen;
 	public UnityEvent init;
 	public NetworkObject playerNetworkObject;
 	
@@ -93,9 +94,6 @@ public class MultiManager : Singleton<MultiManager>
 		NetworkManager.Singleton.OnConnectionEvent += (manager, data) =>
 		{
 			Debug.Log("Client connected");
-			
-			
-			
 		};
 
 	}
@@ -134,7 +132,21 @@ public class MultiManager : Singleton<MultiManager>
 			MultiManager.instance.JoinRelay(_lobby.Data["startGame"].Value);
 		}
 		
-		refreshUI.Invoke();
+		if (_IsOwnerOfLobby)
+		{
+			foreach (Player player in _lobby.Players)
+			{
+				if (player.Data["CharacterID"].Value == "-1")
+				{
+					Debug.Log("false");
+					refreshUI.Invoke(false);
+					return;
+				} 
+			}
+		}
+		
+		Debug.Log("True");
+		refreshUI.Invoke(true);
 	}
 	
 	/// <summary>
@@ -178,6 +190,7 @@ public class MultiManager : Singleton<MultiManager>
 		{
 			Debug.Log(e);
 		}
+		Debug.Log("joined lobby");
 	}
 
 	/// <summary>
@@ -396,18 +409,71 @@ public class MultiManager : Singleton<MultiManager>
 			id: AuthenticationService.Instance.PlayerId,
 			data: new Dictionary<string, PlayerDataObject>
 			{
-				{ "Name", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, _playerName) }
+				{ "Name", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, _playerName) },
+				{ "CharacterID", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, "-1") },
+				{ "IsReady", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Member, "0") }
 			}
 		);
 	}
 
 	/// <summary>
-	/// Gets the localPlayer's GameObject (ie. the character)
+	/// Async method to changes player values in lobby
 	/// </summary>
-	/// <returns></returns>
+	/// <param name="characterID"></param>
+	/// <param name="isReady"></param>
+	public async void UpdatePlayer(int characterID, bool isReady)
+	{
+		try
+		{
+			UpdatePlayerOptions options = new UpdatePlayerOptions
+			{
+				Data = new Dictionary<string, PlayerDataObject>()
+				{
+					{
+						"CharacterID", new PlayerDataObject(
+							visibility: PlayerDataObject.VisibilityOptions.Member,
+							value: characterID.ToString())
+					}
+				}
+			};
+			
+			string playerId = AuthenticationService.Instance.PlayerId;
+
+			await SendLobbyUpdate(playerId, options);
+		}
+		catch (LobbyServiceException e)
+		{
+			Debug.Log(e);
+		}
+	}
+
+	private async Task SendLobbyUpdate(string playerId, UpdatePlayerOptions options)
+	{
+		Debug.Log("Wait");
+		await Task.Delay(100);
+
+		if (_lobby == null)
+		{
+			SendLobbyUpdate(playerId, options);
+			return;
+		}
+		
+		await LobbyService.Instance.UpdatePlayerAsync(_lobby.Id, playerId, options);
+		CharacterChosen.Invoke();
+	}
+
+	/// <summary>
+	/// Gets the localPlayer's GameObject (ie. the character) if not then returns Null
+	/// </summary>
+	/// <returns> GameObject </returns>
 	public GameObject GetPlayerGameObject()
 	{
-		return NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().gameObject;
+		if (NetworkManager.Singleton.SpawnManager != null)
+		{
+			return NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().gameObject;
+		}
+
+		return null;
 	}
 	
 	public List<GameObject> GetAllPlayerGameObjects()
