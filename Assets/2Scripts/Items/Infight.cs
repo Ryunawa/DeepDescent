bdd96adf7ccd60a809d1a75ee161afcdf298ffe2
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2024-06-23 03:54:14 +0200

    Fixed inventory Bug - Fixed enemy not dealing damage

--- Assets/2Scripts/Items/Infight.cs
+++ Assets/2Scripts/Items/Infight.cs
@@ -11,7 +11,7 @@ public class Infight : MonoBehaviour
     private bool _swinging;
     private bool _canInflictDamage = true;
     [SerializeField] private float damageCooldown = 1f;
-
+    [SerializeField] private GameObject parentOwner;
     [SerializeField] private bool isEnemy;
 
     [SerializeField]
@@ -56,7 +56,7 @@ public class Infight : MonoBehaviour
         // Is enemy attacking
         if (_swinging && _canInflictDamage && isEnemy && other.gameObject.CompareTag("Player"))
         {
-            if (TryGetComponent(out EnemyData data))
+            if (parentOwner.TryGetComponent(out EnemyData data))
             {
                 Debug.Log("DAMAGE: " + data.damageInflicted);
                 collidedHealthComponent.TakeDamage(data.damageInflicted);
