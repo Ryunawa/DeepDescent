a5a69481d64e93ecfb31bb0c744a27b156e9afb7
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2024-06-24 20:51:17 +0200

    Redrawed inventory after removing items from quickslot

--- Assets/2Scripts/Entities/Player/PlayerBehaviour.cs
+++ Assets/2Scripts/Entities/Player/PlayerBehaviour.cs
@@ -9,6 +9,7 @@ using UnityEngine.Serialization;
 using NaughtyAttributes;
 using Unity.VisualScripting;
 using System.Collections;
+using _2Scripts.UI;
 
 namespace _2Scripts.Entities.Player
 {
@@ -365,6 +366,7 @@ namespace _2Scripts.Entities.Player
                 int itemId = inventory.QuickSlots[(int)index].ID;
                 inventory.UnEquipQuickSlot((int) index);
                 inventory.InventoryItems.Remove(inventory.InventoryItems.Find(x => x.ID == itemId));
+                GameManager.GetManager<InventoryUIManager>().DrawInventory();
             }
         }
         
