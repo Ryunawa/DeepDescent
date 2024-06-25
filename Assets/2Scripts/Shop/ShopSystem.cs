89bb33a8b29c9dba89595ebdf06bf3b124b57aaa
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2024-06-25 22:39:59 +0200

    Shop ui working and shop working

--- Assets/2Scripts/Shop/ShopSystem.cs
+++ Assets/2Scripts/Shop/ShopSystem.cs
@@ -16,6 +16,7 @@ public class ShopSystem : GameManagerSync<ShopSystem>
     private HashSet<PlayerBehaviour> nearbyPlayers = new HashSet<PlayerBehaviour>();
 
     [SerializeField] private GameObject itemUIPrefab; // Prefab for ItemUI
+    private bool _alreadyPopulated;
 
     protected override void OnGameManagerChangeState(GameState gameState)
     {
@@ -42,6 +43,8 @@ public class ShopSystem : GameManagerSync<ShopSystem>
             return;
         }
 
+        if (_alreadyPopulated)
+            return;
         // Fill weapon UI
         CreateAndSetupItemUI(itemManager.weaponList.Items, GameManager.GetManager<InventoryUIManager>().weaponUIParent);
 
@@ -53,6 +56,8 @@ public class ShopSystem : GameManagerSync<ShopSystem>
 
         // Fill parchment UI
         CreateAndSetupItemUI(itemManager.parchmentList.Items, GameManager.GetManager<InventoryUIManager>().parchmentUIParent);
+
+        _alreadyPopulated = true;
     }
 
     private void CreateAndSetupItemUI(List<Item> items, Transform parent)
